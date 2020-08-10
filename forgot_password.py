7f49bb2d9479fb367ae741dde99e4b8e5c335776
vitor-amartins <vmam@cin.ufpe.br>
2020-08-10 10:15:19 -0300

    Added function of forgot password

---
+++ forgot_password.py
@@ -0,0 +1,26 @@
+import botocore
+import os
+
+from get_secret_hash import get_secret_hash
+
+
+def forgot_password(email: str, client: botocore.client.BaseClient):
+    try:
+        response = client.forgot_password(
+            ClientId=os.environ['CLIENT_ID'],
+            SecretHash=get_secret_hash(email),
+            Username=email,
+        )
+        return {
+            'error': False,
+            'success': True,
+            'message': None,
+            'data': response
+        }
+    except Exception as e:
+        return {
+            'error': True,
+            'success': False,
+            'message': str(e),
+            'data': None
+        }
