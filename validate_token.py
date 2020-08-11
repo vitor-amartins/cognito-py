355981652618990c809cc32bfb72f1d79611714c
vitor-amartins <vmam@cin.ufpe.br>
2020-08-11 11:12:40 -0300

    Adjusted returns of validate_token function

--- validate_token.py
+++ validate_token.py
@@ -9,41 +9,69 @@ load_dotenv()
 
 
 def validate_token(id_token: str):
-    url = f"https://cognito-idp.{os.environ['REGION']}.amazonaws.com/{os.environ['USER_POOL_ID']}/.well-known/jwks.json"
+    try:
+        url = f"https://cognito-idp.{os.environ['REGION']}.amazonaws.com/{os.environ['USER_POOL_ID']}/.well-known/jwks.json"
 
-    with urllib.request.urlopen(url) as f:
-        response = f.read()
-    keys = json.loads(response.decode('utf-8'))['keys']
+        with urllib.request.urlopen(url) as f:
+            response = f.read()
+        keys = json.loads(response.decode('utf-8'))['keys']
 
-    headers = jwt.get_unverified_headers(id_token)
+        headers = jwt.get_unverified_headers(id_token)
 
-    kid = headers['kid']
+        kid = headers['kid']
 
-    key_index = -1
-    for i in range(len(keys)):
-        if kid == keys[i]['kid']:
-            key_index = i
-            break
-    if key_index == -1:
-        # Public key not found in jwks.json
-        return False
+        key_index = -1
+        for i in range(len(keys)):
+            if kid == keys[i]['kid']:
+                key_index = i
+                break
+        if key_index == -1:
+            # Public key not found in jwks.json
+            return {
+                'error': True,
+                'success': False,
+                'message': 'Public key not found in jwks.json',
+                'data': None
+            }
 
-    public_key = jwk.construct(keys[key_index])
+        public_key = jwk.construct(keys[key_index])
 
-    message, encoded_signature = str(id_token).rsplit('.', 1)
+        message, encoded_signature = str(id_token).rsplit('.', 1)
 
-    decoded_signature = base64url_decode(encoded_signature.encode('utf-8'))
+        decoded_signature = base64url_decode(encoded_signature.encode('utf-8'))
 
-    if not public_key.verify(message.encode("utf8"), decoded_signature):
-        # Signature verification failed
-        return False
+        if not public_key.verify(message.encode("utf8"), decoded_signature):
+            # Signature verification failed
+            return {
+                'error': True,
+                'success': False,
+                'message': 'Signature verification failed',
+                'data': None
+            }
 
-    # Signature successfully verified
+        # Signature successfully verified
 
-    claims = jwt.get_unverified_claims(id_token)
+        claims = jwt.get_unverified_claims(id_token)
 
-    if time.time() > claims['exp']:
-        # Token is expired
-        return False
+        if time.time() > claims['exp']:
+            # Token is expired
+            return {
+                'error': True,
+                'success': False,
+                'message': 'Token is expired',
+                'data': None
+            }
 
-    return claims
+        return {
+            'error': False,
+            'success': True,
+            'message': None,
+            'data': claims
+        }
+    except Exception as e:
+        return {
+            'error': True,
+            'success': False,
+            'message': str(e),
+            'data': None
+        }
